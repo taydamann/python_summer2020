8fc6dff83f0d39a7ed7535e1725b77dc926e6ab2
Taylor Damann <taydamann@yahoo.com>
2020-08-28 09:51:23 -0500

    Class work

--- Day2/ClassExercise/day02_exercise.py
+++ Day2/ClassExercise/day02_exercise.py
@@ -1,16 +1,22 @@
 # An exercise 
 
 class Senator():
-  def __init__(self, name):
-    self.name = name
-    self.bills_voted_on = [] ## list of Bill objects
+    def __init__(self, name):
+        self.name = name
+        self.bills_voted_on = [] ## list of Bill objects
 
-  def __str__(self): # Print method  
+    def __str__(self): # Print method  
+        return "%s\n%s" %(self.name, self.bills_voted_on)
 
-  def vote(self, bill, choice):
+    def vote(self, bill, choice):
     #update the bill object--add the senator's name to the the list of yes/no/abstain
+        bill.votes[self.choice].append(self.name)
+    
     #update the senator object--add this bill to the bills this senator has voted on
+        self.bills_voted_on.append(bill)
+    
     #print an informative message announcing the vote 
+        print("Senator" + self.name  + "voted" + self.choice + "on bill" + bill.title)
 
 class Bill():
   def __init__(self, title):
@@ -18,9 +24,9 @@ class Bill():
     self.votes = {"yes" : [], "no" : [], "abstain" : []}
     self.passed = None
 
-  def __str__(self): # Print method  
+  #def __str__(self): # Print method  
 
-  def result(self):
+  #def result(self):
     ## update and return the "passed" variable to indicate True/False if the bill passed
 
 
--- Day2/Lab/lab02.py
+++ Day2/Lab/lab02.py
@@ -7,21 +7,50 @@ class Clock:
 
     ## Print the time
     def __str__(self):
+        return str(self.hour) + ":" + str(self.minutes).zfill(2)
     
     ## Add time
     ## Don't return anything
     def __add__(self,minutes):
+        addTime = self.hour*60 + self.minutes + minutes
+        
+        if addTime > 24*60:
+            addTime = addTime - 24*60
+        
+        else :
+            addTime = addTime
+        
+        self.hour = addTime // 60
+        self.minute = addTime % 60
     
     ## Subtract time
     ## Don't return anything
     def __sub__(self,minutes):
+        subTime = self.hour*60 + self.minutes - minutes
+        
+        if subTime > 24*60:
+            subTime = subTime - 24*60
+        elif subTime < 0:
+            subTime = subTime + 24*60
+        else :
+            subTime = subTime
+        
+        self.hour = subTime // 60
+        self.minute = subTime % 60
     
     ## Are two times equal?
     def __eq__(self, other):
+        if self.hour == other.hour and self.minutes == other.minutes:
+            return True
+        else:
+            return False
     
     ## Are two times not equal?
     def __ne__(self, other):
-
+        if self.hour == other.hour and self.minutes == other.minutes:
+            return False
+        else:
+            return True
 
 # You should be able to run these
 clock1 = Clock(23, 5)
